297837b5f1156d3f86fec4b13c8cc16626b2bfd1
springleo <782655835@qq.com>
2019-04-17 16:38:23 +0800

    feat： add icon in development

--- src/app.vue
+++ src/app.vue
@@ -87,8 +87,8 @@
                     <u-table-column label="分类" width="100px">{{ row.tab }}</u-table-column>
                     <u-table-column label="名称" width="100px">{{ row.title }}</u-table-column>
                     <u-table-column label="操作" width="60px">
-                        <u-link name="edit" />
-                        <u-link name="delete" />
+                        <u-icon name="edit" />
+                        <u-icon name="delete" />
                     </u-table-column>
                 </template>
             </u-table>
@@ -101,11 +101,6 @@
 
 <script>
 import DComponentItem from '@/components/demo/d-component-item'
-// import DModalTest from '@/components/demo/d-modal-test'
-// import DModalForm from '@/components/demo/d-modal-form'
-// import { getAllTopic } from '@/server'
-// import { transfer } from '@/utils/helper'
-
 const l = console.log
 
 export default {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import app from './app.vue'
 import * as YIUI from '../index'
+
+// 本地icon
+import * as svgicon from 'vue-svgicon'
+import '@/assets/icon'
+Vue.use(svgicon, { tagName: 'svgicon', width: 100, height: 100 })
+import UIcon from './components/u-icon'
+Vue.component('UIcon', UIcon)
+
 Object.keys(YIUI).forEach(key => {
     Vue.component(key, YIUI[key])
     key === 'UToast' && (Vue.prototype.$toast = YIUI[key].toast)
